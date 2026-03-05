2cbee6fa65dbc15fd0deb968293e5196bb7d956c
MrEstSail <kristoferlaast@gmail.com>
2026-03-05 15:58:21 +0200

    Add openingBook object with chess openings

--- openings.js
+++ openings.js
@@ -1,1 +1,4 @@
-
+const openingBook = {
+  "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1": "King's Pawn Opening",
+  "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2": "Open Game"
+};
