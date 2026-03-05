f39bb7e84db11d09b39edafa404aa223a60e90ae
MrEstSail <kristoferlaast@gmail.com>
2026-03-05 16:06:24 +0200

    Refactor openingBook to use new format

--- openings.js
+++ openings.js
@@ -1,4 +1,15 @@
 const openingBook = {
-  "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1": "King's Pawn Opening",
-  "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2": "Open Game"
-};
+
+"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1":
+"King's Pawn Opening",
+
+"rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2":
+"Open Game",
+
+"rnbqkbnr/pppppppp/8/8/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 1":
+"Center Game",
+
+"rnbqkbnr/pppppppp/8/8/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 1":
+"King's Knight Opening"
+
+}
